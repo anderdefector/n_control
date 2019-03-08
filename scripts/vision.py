957e18bac4574cf1ce3ba996a83b205cd2511519
Andres Cureño <ander.cr@outlook.com>
2019-03-08 09:31:36 -0600

    Add files via upload

--- scripts/vision.py
+++ scripts/vision.py
@@ -9,23 +9,34 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+def nothing(x):
+  pass
+
 class image_converter:
 
   def __init__(self):
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/ardrone/front/image_raw",Image,self.callback)
+    # self.image_sub = rospy.Subscriber("/ardrone/front/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/cv_camera/image_raw",Image,self.callback)
 
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv2.namedWindow("Image window")
+
+      grayscaled = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+
+      cv2.createTrackbar("INV","Image window",0,255,nothing)
+
+      Mask = cv2.getTrackbarPos("INV","Image window")
+
+      retval, threshold = cv2.threshold(grayscaled, Mask, 255, cv2.THRESH_BINARY_INV)
+
+      cv2.imshow("Image window", threshold)
+
     except CvBridgeError as e:
       print(e)
-    
-
-    grayscaled = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
-    retval, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY_INV)
-    
 
     cv2.imshow("Image window", threshold)
     cv2.waitKey(3)
