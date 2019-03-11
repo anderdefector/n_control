cea4d1e0ec91831208ad8258545151fc01224e5f
Andres <anderdefector@gmail.com>
2019-03-11 14:12:27 -0600

    Actualización Nodo Visión
    - Se agregaron los comandos para publicar los datos MX, MY y el Radio.
    
    - Se agregó la funcion para detectar contornos
    
    - Se agregó la función para encontrar los momentos del área más grande
    
    - Se agregaron funciones para desplegar la información en la imagen

--- scripts/vision.py
+++ scripts/vision.py
@@ -3,9 +3,11 @@ from __future__ import print_function
 
 import sys
 import rospy
+import math
 import cv2
 import numpy as np
 from std_msgs.msg import String
+from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -15,7 +17,10 @@ def nothing(x):
 class image_converter:
 
   def __init__(self):
-
+    self.MX = rospy.Publisher("/MX",Int32,queue_size=10)
+    self.MY = rospy.Publisher("/MY",Int32,queue_size=10)
+    self.Radio = rospy.Publisher("/Radio",Int32,queue_size=10)
+    
     self.bridge = CvBridge()
     # self.image_sub = rospy.Subscriber("/ardrone/front/image_raw",Image,self.callback)
     self.image_sub = rospy.Subscriber("/cv_camera/image_raw",Image,self.callback)
@@ -23,22 +28,62 @@ class image_converter:
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-      cv2.namedWindow("Image window")
+      cv2.namedWindow("Invertida")
+      cv2.namedWindow("Original")
 
       grayscaled = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
 
-      cv2.createTrackbar("INV","Image window",0,255,nothing)
+      cv2.createTrackbar("INV","Invertida",0,255,nothing)
 
-      Mask = cv2.getTrackbarPos("INV","Image window")
+      Mask = cv2.getTrackbarPos("INV","Invertida")
 
       retval, threshold = cv2.threshold(grayscaled, Mask, 255, cv2.THRESH_BINARY_INV)
 
-      cv2.imshow("Image window", threshold)
+      im2, contours, hierarchy = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+      font = cv2.FONT_HERSHEY_SIMPLEX
+
+      if(len(contours) == 0):
+        cv2.putText(cv_image,'NO HAY CONTORNOS',(15,30), font, 1,(255,0,0),2,cv2.LINE_AA)
+      else:
+        #Obtenecion del area maxima encontrada con los contornos
+        area_max = 0.0
+        for x in range(0, len(contours)):
+          area = cv2.contourArea(contours[x])
+          if (area > area_max):
+            area_max = area
+            d = x
+        #Momento del area maxima
+        M = cv2.moments(contours[d])
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        #Radio del area y esquinas del rectangulo
+        r = int(math.sqrt( area_max/3.1416 ))
+        y=(r)*(math.sin(math.radians(45)))
+        x=(r)*(math.cos(math.radians(45)))
+        x1 = int(cx - x)
+        x2 = int(cx + x)
+        y1 = int(cy - y)
+        y2 = int(cy + y)
+        #Presentacion datos sobre imagen
+        texto = "Cx = " + str(cx) + " Cy = " + str(cy)
+        cv2.putText(cv_image,texto,(15,30), font, 1,(255,0,0),2,cv2.LINE_AA)
+        cv2.drawContours(cv_image, contours[d], -1, (255,0,0), 3)
+        cv2.rectangle(cv_image,(x1,y1),(x2,y2),(0,255,0),3)
+        cv2.circle(cv_image,(cx,cy), r, (0,255,0), 3)
+        cv2.circle(cv_image,(cx,cy), 7, (255,255,0), -1)
+
+        #Publicacion de mensaje
+        self.MX.publish(cx)
+        self.MY.publish(cy)
+        self.Radio.publish(r)
+
+      cv2.imshow("Invertida", threshold)
+      cv2.imshow("Original", cv_image)
 
     except CvBridgeError as e:
       print(e)
 
-    cv2.imshow("Image window", threshold)
     cv2.waitKey(3)
 
 def main(args):
